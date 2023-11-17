616ab39d7d1286c516bf35b3bb8e41b4b6497289
Antonio Henrique Machado <96703665+machadoah@users.noreply.github.com>
2023-11-17 02:05:34 -0300

    insert method post to product

--- src/main/java/com/medium/machadoah/springboot/controllers/ProductController.java
+++ src/main/java/com/medium/machadoah/springboot/controllers/ProductController.java
@@ -1,14 +1,32 @@
 package com.medium.machadoah.springboot.controllers;
 
+import com.medium.machadoah.springboot.dto.ProductRecordDTO;
+import com.medium.machadoah.springboot.models.ProductModel;
 import com.medium.machadoah.springboot.repositories.ProductRepository;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class ProductController {
 
-    ProductRepository productRepository;
+    private final ProductRepository productRepository;
 
+    @PostMapping("/products")
+    public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDTO productRecordDTO) {
+        // instanciando um objeto do tipo ProductModel
+        var productModel = new ProductModel();
 
+        // 🚨🚨 copiando as propriedades de productRecordDTO para productModel
+        BeanUtils.copyProperties(productRecordDTO, productModel);
+
+        // salvando o produto no banco de dados
+        return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
+    }
 }
