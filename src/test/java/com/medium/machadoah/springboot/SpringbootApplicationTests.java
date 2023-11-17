0634681ced183d8967aaf617e67e0a69ab81bcff
Antonio Henrique Machado <96703665+machadoah@users.noreply.github.com>
2023-11-17 01:06:31 -0300

    rename application

--- src/test/java/com/medium/machadoah/springboot/SpringbootApplicationTests.java
+++ src/test/java/com/medium/machadoah/springboot/SpringbootApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CursoSpring3ApplicationTests {
+class SpringbootApplicationTests {
 
     @Test
     void contextLoads() {
