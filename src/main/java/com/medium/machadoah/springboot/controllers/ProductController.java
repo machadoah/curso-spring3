fd11fab938afabb89f8a6f34b2a1895a88b5c847
Antonio Henrique Machado <96703665+machadoah@users.noreply.github.com>
2023-11-17 02:09:01 -0300

    insert method get all products

--- src/main/java/com/medium/machadoah/springboot/controllers/ProductController.java
+++ src/main/java/com/medium/machadoah/springboot/controllers/ProductController.java
@@ -8,9 +8,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -29,4 +32,10 @@ public class ProductController {
         // salvando o produto no banco de dados
         return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
     }
+
+    @GetMapping("/products")
+    public ResponseEntity<List<ProductModel>> getAllProducts() {
+        return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll());
+    }
+
 }
