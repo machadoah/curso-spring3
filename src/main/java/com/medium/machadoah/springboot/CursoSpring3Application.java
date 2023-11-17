28be18b40e686305d32f50ea4697b11ac6cfa860
Antonio Henrique Machado <96703665+machadoah@users.noreply.github.com>
2023-11-17 00:56:57 -0300

    init Springboot project

---
+++ src/main/java/com/medium/machadoah/springboot/CursoSpring3Application.java
@@ -0,0 +1,13 @@
+package com.medium.machadoah.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpring3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CursoSpring3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/medium/machadoah/springboot/CursoSpring3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medium.machadoah.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpring3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
