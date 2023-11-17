6abd5cb05551ea3e5c2d3bede9b45e37a9f76362
Antonio Henrique Machado <96703665+machadoah@users.noreply.github.com>
2023-11-17 01:34:03 -0300

    create repository with jpa repository

---
+++ src/main/java/com/medium/machadoah/springboot/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.medium.machadoah.springboot.repositories;
+
+import com.medium.machadoah.springboot.models.ProductModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
+}
